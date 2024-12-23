abda7be846d0ea32826ad27aa1c301ac63f9b31f
TaizunJ <jafri.taizun.s@gmail.com>
2024-12-23 08:37:38 -0800

    No changes

--- Firmware/ESP32/ESP32_DAQC/src/main.cpp
+++ Firmware/ESP32/ESP32_DAQC/src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
-
+#include "adxl375.h"
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600); // Enabling serial communication
+  ADXL375_init(); // Initializing the ADXL375 accelerometer
+
 }
 
 void loop() {
