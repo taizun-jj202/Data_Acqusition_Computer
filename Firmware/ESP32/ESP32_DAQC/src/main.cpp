bb87c2c0e5b9aff700a052049825bacf1e754aec
TaizunJ <jafri.taizun.s@gmail.com>
2024-12-23 11:41:09 -0800

    ADXL375 Sensore firmware completed

--- Firmware/ESP32/ESP32_DAQC/src/main.cpp
+++ Firmware/ESP32/ESP32_DAQC/src/main.cpp
@@ -1,20 +1,38 @@
+/**
+ * @file main.cpp
+ * @brief Main firmware for ESP32 DAQC computer.
+ * 
+ * This file contains the main firmware for the ESP32 DAQC computer.
+ * Firmware has the following functionalities:
+ * - Initialize all sensors.
+ * - Read data from sensors.
+ * - Store data either on built-in flash memory or SD card. 
+ *    - SD card data storage is prioritized over built-in flash memory.
+ */
+
 #include <Arduino.h>
 #include "adxl375.h"
-// put function declarations here:
-int myFunction(int, int);
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600); // Enabling serial communication
   ADXL375_init(); // Initializing the ADXL375 accelerometer
 
 }
 
+// Initialization variables :
+    // ADXL375 accelerometer variables. 
+  int16_t x_acc;
+  int16_t y_acc;
+  int16_t z_acc;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+
+  // Reading ADX375 accelerometer data
+  ADXL375_read_acceleration(&x_acc, &y_acc, &z_acc);
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
